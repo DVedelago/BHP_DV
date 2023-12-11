9c9dcecea4c9bcafac5bd30d802b6ba942317252
DVedelago_IIT <diego.vedelago@iit.it>
2023-12-11 17:58:19 +0100

    cosmetics, upload PDF

--- CH2/Netcat.py
+++ CH2/Netcat.py
@@ -25,15 +25,11 @@ class NetCat:
       self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-
-
    def run(self):
       if self.args.listen:
          self.listen()
       else:
-         self.send()
-   
-
+         self.send() 
 
    def send(self):
       self.socket.connect((self.args.target, self.args.port))
@@ -62,8 +58,6 @@ class NetCat:
          self.socket.close()
          sys.exit()
 
-
-
    def listen(self):
       self.socket.bind((self.args.target, self.args.port))
       self.socket.listen(5)
@@ -71,8 +65,6 @@ class NetCat:
          client_socket, _ = self.socket.accept()
          client_thread = threading.Thread(target=self.handle, args=(client_socket))
          client_thread.start()
-
-
 
    def handle(self, client_socket):
       if self.args.execute:
