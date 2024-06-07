adeb330c9808748e628ee7b8be3f0128c413b3d7
DVedelago <diego.vedelago@iit.it>
2024-06-07 16:07:40 +0200

    cosmetics

--- CH2/Netcat.py
+++ CH2/Netcat.py
@@ -11,12 +11,14 @@ def execute(cmd):
     if not cmd:
         return
     
-    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)          # return the output as a string
     return output.decode()
 
 
 
-
+#######################################
+# NetCat class and methods definition #
+#######################################
 
 class NetCat:
    def __init__(self, args, buffer=None):
@@ -106,12 +108,15 @@ class NetCat:
 #*********************************************************************#
 
 
+##############
+# Main Block #
+##############
 
 if __name__ == '__main__': 
-   parser = argparse.ArgumentParser(
+   parser = argparse.ArgumentParser(                                                       # (1) Create a CLI to provide arguments
       description='BHP Net Tool',
-      formatter_class=argparse.RawDescriptionHelpFormatter,
-      epilog=textwrap.dedent('''Example:
+      formatter_class=argparse.RawDescriptionHelpFormatter,                                # (2) --help invoking
+      epilog=textwrap.dedent('''Example:                                                  
                              
                              netcat.py -t 192.168.1.108 -p 5555 -l -c                           
                              #command shell
@@ -128,16 +133,16 @@ if __name__ == '__main__':
                              netcat.py -t 192.168.1.108 -p 5555
                              #connect to server
                              '''))
-   parser.add_argument('-c', '--command', action='store_true', help='Command_shell')
-   parser.add_argument('-e', '--execute', help='Execute specified command')
-   parser.add_argument('-l', '--listen', action='store_true', help='Listen')
-   parser.add_argument('-p', '--port', type=int, default=5555, help='Specified port')
-   parser.add_argument('-t', '--target', default='192.168.1.203', help='Specified IP')
-   parser.add_argument('-u', '--upload', help='Upload file')
+   parser.add_argument('-c', '--command', action='store_true', help='Command_shell')      # (3) Setup interactive shell w/ -c
+   parser.add_argument('-e', '--execute', help='Execute specified command')               # -e execute one specific command
+   parser.add_argument('-l', '--listen', action='store_true', help='Listen')              # -l listener should be set up
+   parser.add_argument('-p', '--port', type=int, default=5555, help='Specified port')     # -p specifies the port on which communicate
+   parser.add_argument('-t', '--target', default='192.168.1.203', help='Specified IP')    # -t specifies the target IP
+   parser.add_argument('-u', '--upload', help='Upload file')                              # -u specifies the name of a file to upload 
 
    args = parser.parse_args()
 
-   if args.listen:
+   if args.listen:                                                                        # (4) if setting up a listener, invoke NetCat obj
       buffer = ''
    else:
       buffer = sys.stdin.read()
