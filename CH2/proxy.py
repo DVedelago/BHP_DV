764145535de64a09db8e24794b457992a0161b66
DVedelago <diego.vedelago@iit.it>
2026-09-13 12:11:48 +0200

    WIP proxy.py

--- CH2/proxy.py
+++ CH2/proxy.py
@@ -2,8 +2,11 @@ import sys
 import socket
 import threading
 
-HEX_FILTER= ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
 
+# Creates a LUT for ASCII printable and non-printable characters by checking the length
+
+# HEX_FILTER= ''.join([(len(repr(chr(i))) == 3) and chr(i) or '.' for i in range(256)])
+HEX_FILTER= ''.join(chr(i) if len(repr(chr(i))) == 3 else '.')
 def hexdump(src, length=16, show=True):
     if isinstance(src, bytes):
         src = src.decode()
@@ -14,10 +17,74 @@ def hexdump(src, length=16, show=True):
 
         printable = word.translate(HEX_FILTER)
         hexa = ' '.join([f'{ord(c):02X}' for c in word])
-        hexwidth = lentgh * 3
+        hexwidth = length * 3
         results.append(f'{i:04X} {hexa:<{hexwidth}} {printable}')
     if show:
         for line in results:
             print(line)
     else:
         return results
+
+def receive_from(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except Exception as e:
+        pass
+    return buffer
+
+
+# Placeholder for packets intervention/modification
+
+def request_handler(buffer):
+    # do stuff
+    return buffer
+
+def response_handler(buffer):
+    # do stuff
+    return buffer
+
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+    remote_buffer = response_handler(remote_buffer)
+    if len(remote_buffer):
+        print("[<==] Sending %d bytes to localhost." % len(remote_buffer))
+        client_socket.send(remote_buffer)
+
+    while True:
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            line = "[==>] Received %d bytes from localhost." % len(local_buffer)
+            print(line)
+            hexdump(local_buffer)
+
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print("[==>] Sent to remote.")
+        
+    remote_buffer = receive_from(remote_socket)
+    if len(remote_buffer):
+        print("[<==] Received %d bytes from remote." % len(remote_buffer))
+        hexdump(remote_buffer)
+
+        remote_buffer = response_handler(remote_buffer)
+        client_socket.send(remote_buffer)
+        print("[<==] Sent to localhost.")
+
+    if not len(local_buffer) or not len(remote_buffer):
+        client_socket.close()
+        remote_socket.close()
+        print("[*] No more data. Closing connections.")
+        break
+    
