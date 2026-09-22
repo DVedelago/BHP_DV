d555ef21ad7fd02959a004259f4c835cf99dfee4
DVedelago <diego.vedelago@iit.it>
2026-09-22 23:27:11 +0200

    proxy py wip 2

--- CH2/proxy.py
+++ CH2/proxy.py
@@ -49,6 +49,8 @@ def response_handler(buffer):
     # do stuff
     return buffer
 
+# End placeholder
+
 def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     remote_socket.connect((remote_host, remote_port))
@@ -87,4 +89,56 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
         remote_socket.close()
         print("[*] No more data. Closing connections.")
         break
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))
+    except Exception as e:
+        print('Problem on bind: %r' % e)
+
+        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions.")
+        sys.exit(0)
     
+    print("[*] Listening on %S:%d" % (local_host, local_port))
+    server.listen(5)
+    while True:
+        client_socket, addr = server.accept()
+        
+        # Print info on local connection
+        line = "> Received incoming connection from %s:%d" %(addr[0], addr[1])
+        print(line)
+
+        # Start a dedicated thread for remote host dialogueing
+        proxy_thread = threading.Thread(
+            target=proxy_handler,
+            args=(client_socket, remote_host, remote_port, receive_first))
+        proxy_thread.start()
+
+
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./proxy.py [localhost] [localport]", end='')
+        print("[remotehost] [remoteport] [receive_first]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 90000 True")
+        sys.exit()
+
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+
+    receive_first = sys.argv[5]
+
+    if "True" in receive_first:
+        receive_first = True
+    else:
+        receive_first = False
+
+    server_loop(local_host, local_port, remote_host, remote_port, receive_first)
+
+
+if __name__ == '__main__':
+    main()
